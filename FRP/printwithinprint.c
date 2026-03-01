c902aaa6c0dae24725ebc3f6057e3c7ef6ad2920
Soumyadeep Haldar <soumyadeephaldar2006@gmail.com>
2026-03-01 23:52:26 +0530

    Add files via upload

---
+++ FRP/printwithinprint.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("%d",printf("10+20=30"));
+    return 0;
+}
---
+++ FRP/variable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+
+    int a=10,b=20,c=30;
+    printf("%d");   //4200784
+    printf("%p,%p,%p",&a,&b,&c);
+    
+    return 0;
+}
